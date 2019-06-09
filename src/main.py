38f66c4f51a841552c645a2a6c19a05884399cb6
Salvador Guzman <guzmansalv@gmail.com>
2019-06-09 14:51:58 -0700

    simple server

--- src/main.py
+++ src/main.py
@@ -1,31 +1,42 @@
 import atexit
+import flask
 import psycopg2
+
+app: flask.Flask = flask.Flask(__name__)
+user: str = 'admin'
+password: str = ''
+pg_host: str = 'localhost'
+pg_port: str = '5432'
+database: str = 'youtube'
+server_host: str = '0.0.0.0'
+server_port: str = '8081'
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 
 def connect() -> psycopg2:
     print('Connecting to db')
-    user: str = 'admin'
-    password: str = ''
-    host: str = 'localhost'
-    port: str = '5432'
-    database: str = 'youtube'
-
-    return psycopg2.connect(
+    conn: psycopg2 = psycopg2.connect(
         user=user,
         password=password,
-        host=host,
-        port=port,
+        host=pg_host,
+        port=pg_port,
         database=database)
-
-
-def main() -> None:
-    conn: psycopg2 = connect()
 
     def exit_func() -> None:
         print('Closing connection')
         conn.close()
 
     atexit.register(exit_func)
+    return conn
+
+
+def main() -> None:
+    conn: psycopg2 = connect()
+    app.run(host=server_host, port=server_port, debug=True)
 
 
 if __name__ == '__main__':
