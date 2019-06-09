953f0d76e373dc20293273e7a1c4520ccbee66de
Salvador Guzman <guzmansalv@gmail.com>
2019-06-09 14:37:14 -0700

    postgresql

--- src/main.py
+++ src/main.py
@@ -1,5 +1,31 @@
+import atexit
+import psycopg2
+
+
+def connect() -> psycopg2:
+    print('Connecting to db')
+    user: str = 'admin'
+    password: str = ''
+    host: str = 'localhost'
+    port: str = '5432'
+    database: str = 'youtube'
+
+    return psycopg2.connect(
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        database=database)
+
+
 def main() -> None:
-    print('Hello', 'World')
+    conn: psycopg2 = connect()
+
+    def exit_func() -> None:
+        print('Closing connection')
+        conn.close()
+
+    atexit.register(exit_func)
 
 
 if __name__ == '__main__':
