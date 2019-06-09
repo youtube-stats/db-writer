a871123ea96d9f355f1c9bc6caf78b4a6537c52c
Salvador Guzman <guzmansalv@gmail.com>
2019-06-09 16:30:17 -0700

    store when write threshold is hit

--- src/main.py
+++ src/main.py
@@ -15,16 +15,22 @@ class SubRow:
         self.idx: int = idx
         self.subs: int = subs
 
+    def tuple(self):
+        return self.time, self.idx, self.subs
+
 
 class SubStore:
     def __init__(self):
         self.store: List[SubRow] = []
 
-    def append(self, sub: SubRow):
+    def append(self, sub: SubRow) -> None:
         self.store.append(sub)
 
-    def len(self):
+    def len(self) -> int:
         return len(self.store)
+
+    def get(self, i: int) -> SubRow:
+        return self.store[i]
 
 
 app: flask.Flask = flask.Flask(__name__)
@@ -39,26 +45,7 @@ server_port: str = '8081'
 tz = pytz.timezone('America/Los_Angeles')
 write_threshold: int = 500
 store: SubStore = SubStore()
-
-
-def init_ack_message() -> str:
-    msg_obj: message_pb2.Ack = message_pb2.Ack()
-    msg_obj.ok = True
-    return msg_obj.SerializeToString()
-
-
-message_ack = init_ack_message()
-
-
-@app.route('/post', methods=['POST'])
-def hello() -> str:
-    sub_message: message_pb2.SubMessage = message_pb2.SubMessage()
-    sub_message.ParseFromString(flask.request.data)
-    print('Got message', sub_message.SerializeToString())
-
-    message_queue.put_nowait(sub_message)
-
-    return message_ack
+insert_sql: str = 'INSERT INTO youtube.stats.subs VALUES (%s, %s, %s)'
 
 
 def connect() -> psycopg2:
@@ -78,6 +65,29 @@ def connect() -> psycopg2:
     return conn
 
 
+conn: psycopg2 = connect()
+
+
+def init_ack_message() -> str:
+    msg_obj: message_pb2.Ack = message_pb2.Ack()
+    msg_obj.ok = True
+    return msg_obj.SerializeToString()
+
+
+message_ack = init_ack_message()
+
+
+@app.route('/post', methods=['POST'])
+def post() -> str:
+    sub_message: message_pb2.SubMessage = message_pb2.SubMessage()
+    sub_message.ParseFromString(flask.request.data)
+    print('Got message', sub_message.SerializeToString())
+
+    message_queue.put_nowait(sub_message)
+
+    return message_ack
+
+
 def append_to_store(payload: message_pb2.SubMessage) -> None:
     length: int = len(payload.idx)
     time: datetime.datetime = datetime.datetime.fromtimestamp(payload.time, tz)
@@ -90,9 +100,24 @@ def append_to_store(payload: message_pb2.SubMessage) -> None:
         store.append(row)
 
 
+def insert_job() -> None:
+    cursor = conn.cursor()
+    for i in range(write_threshold):
+        sub: SubRow = store.get(i)
+        cursor.execute(insert_sql, sub.tuple())
+
+    conn.commit()
+    cursor.close()
+
+
 def write_payload_handler(payload: message_pb2.SubMessage) -> None:
     append_to_store(payload)
     print('Size of store is ', store.len())
+
+    if store.len() > write_threshold:
+        print('Writing', store.len(), 'rows into database')
+        insert_job()
+        print('New store size is', store.len())
 
 
 def write_daemon() -> None:
@@ -104,7 +129,6 @@ def write_daemon() -> None:
 
 
 def main() -> None:
-    conn: psycopg2 = connect()
     threading.Thread(target=write_daemon, daemon=False).start()
 
     app.run(host=server_host, port=server_port, debug=True)
